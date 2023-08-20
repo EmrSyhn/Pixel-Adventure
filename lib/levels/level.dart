929883f40d37107963efa4ab7c73f5975d98697e
Emir <emirseyhana.z@gmail.com>
2023-08-20 21:28:21 +0300

    first commit

---
+++ lib/levels/level.dart
@@ -0,0 +1,14 @@
+import 'dart:async';
+
+import 'package:flame/components.dart';
+
+class Level extends World {
+
+
+
+ @override
+  FutureOr<void> onLoad() {
+    
+    return super.onLoad();
+  } 
+}
